94d9f60ea35c925eed3480bcecadab80bb4e6d25
Stu Wilcox <stuart_wilcox@outlook.com>
2022-04-07 21:37:12 -0700

    fix footer + complete digital marketing page

--- src/components/Footer/index.style.tsx
+++ src/components/Footer/index.style.tsx
@@ -4,13 +4,26 @@ export const StyledFooter = styled.div`
     display: flex;
     flex-direction: row;
     align-items: center;
+    margin-top: 100px;
 `;
 
 export const StyledFooterShoulder = styled.div`
     flex: 1;
 `;
 
+export const StyledFooterGroup = styled.div`
+    flex: 3;
+    border-top: solid 1px #696767;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+`;
+
 export const StyledFooterItem = styled.div`
     padding: 10px;
     margin: 10px;
+`;
+
+export const StyledFooterIcon = styled.img`
+    
 `;
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,6 +3,8 @@ import {
     StyledFooter,
     StyledFooterShoulder,
     StyledFooterItem,
+    StyledFooterGroup,
+    StyledFooterIcon,
 } from './index.style';
 
 export interface IProps {
@@ -10,20 +12,44 @@ export interface IProps {
 }
 
 const Footer: React.FC<IProps> = (props: IProps) => {
-    const {} = props
+    const {} = props;
+
+    const handleResumeClick = () => {
+        // TODO resume download
+        console.log('Resume download');
+    };
     
     return (
         <StyledFooter>
             <StyledFooterShoulder />
-            <StyledFooterItem>
-                _mail
-            </StyledFooterItem>
-            <StyledFooterItem>
-                _instagram
-            </StyledFooterItem>
-            <StyledFooterItem>
-                _resume
-            </StyledFooterItem>
+            <StyledFooterGroup>
+                <StyledFooterShoulder />
+                <StyledFooterItem>
+                    <a href={'mailto:d.humilde@outlook.com'}>
+                        <StyledFooterIcon
+                            src={'/images/email.png'}
+                            alt={'Email'}
+                        />
+                    </a>
+                </StyledFooterItem>
+                <StyledFooterItem>
+                    <a href={'https://www.linkedin.com/in/danielle-humilde/'} target={'_blank'}>
+                        <StyledFooterIcon
+                            src={'/images/linkedin.png'}
+                            alt={'LinkedIn'}
+                        />
+                    </a>
+                </StyledFooterItem>
+                <StyledFooterItem>
+                    <StyledFooterIcon
+                        style={{ cursor: 'pointer' }}
+                        src={'/images/resume.png'}
+                        alt={'Resume'}
+                        onClick={() => handleResumeClick()}
+                    />
+                </StyledFooterItem>
+                <StyledFooterShoulder />
+            </StyledFooterGroup>
             <StyledFooterShoulder />
         </StyledFooter>
     )
